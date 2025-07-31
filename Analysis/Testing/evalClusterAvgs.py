2686a09925976ca502aa09d5270927484a98d42e
Seth Rojas <sethrojas24@icloud.com>
2025-07-30 17:56:57 -0700

    wasn't just for median bpm was for other stats which I added

--- Analysis/Testing/evalClusterAvgs.py
+++ Analysis/Testing/evalClusterAvgs.py
@@ -6,13 +6,11 @@ import numpy as np
 
 
 conn = sqlite3.connect('rosteriq.db')
-
-bpm_dict = {}  # store dict of bpms 
-bpm_df = pd.DataFrame(columns=['season_year', 'pos', 'team_clu_id', 'player_clu_id', 'bpm'])
-for year in range(2021, 2022):
+ 
+cluster_df = pd.DataFrame(columns=['season_year', 'pos', 'team_clu_id', 'player_clu_id', 'length', 'median', 'std'])
+for year in range(2022, 2023):
 
     team_df = pd.read_csv('Analysis/Clustering/15ClusterData/2021/KClustering/labels.csv')
-    bpm_dict[year] = {}
     for pos in Config.POSITIONS:
         pos_df = pd.read_csv(f'Analysis/Clustering/Players/2021/KClustering/player_labels_{pos}.csv')
 
@@ -35,46 +33,22 @@ for year in range(2021, 2022):
         team_cluster_ids = sorted(merged_df['team_cluster'].unique())
         player_cluster_ids = sorted(merged_df['Cluster'].unique())
 
-        bpm_dict[year][pos] = {}
+        
         # double for to get bpm on each cluster
         for team_id in team_cluster_ids:
-            bpm_dict[year][pos][team_id] = {}
             for player_id in player_cluster_ids:
                 filtered_df = merged_df[(merged_df['Cluster'] == player_id) & (merged_df['team_cluster'] == team_id)]
-                bpm = filtered_df['bpm'].median()  # bpm of player and team cluster
-                bpm_dict[year][pos][team_id][player_id] = bpm
-                bpm_df.loc[len(bpm_df)] = [year, pos, team_id, player_id, bpm]
+                bpm = filtered_df['bpm']
+                length = len(bpm)
+                median = bpm.median()  # bpm of player and team cluster
+                std = bpm.std()
+
+                cluster_df.loc[len(cluster_df)] = [year, pos, team_id, player_id, length, median, std]
         
         print(pos)
-        print("Length of team cluster ids", len(team_cluster_ids))
-        print("Length of player cluster ids", len(player_cluster_ids), player_cluster_ids)
-
-        plyr_cluster_diff = (
-            bpm_df[bpm_df['pos'] == pos]
-            .groupby(['season_year','player_clu_id'], as_index=False)
-            .agg(
-                median_bpm=('bpm', 'median'),
-                std_dev=('bpm', 'std'),
-                range=('bpm', lambda x: x.max() - x.min()),
-            )
-        )
-        print(plyr_cluster_diff)
-
-    # Calculate within-team BPM differences without FutureWarning
-    within_team_diff = (
-        bpm_df
-        .groupby(['season_year', 'pos', 'team_clu_id'], as_index=False)
-        .agg(
-            mean_bpm=('bpm', 'mean'),
-            std_dev=('bpm', 'std'),
-            range=('bpm', lambda x: x.max() - x.min()),
-            pct_range=('bpm', lambda x: ((x.max() - x.min()) / abs(x.mean()) * 100)
-                                        if x.mean() != 0 else 0)
-        )
-    )
-
-    print("Within-team BPM differences:")
-    print(within_team_diff)
+        for player_id in player_cluster_ids:
+            plyr_clu = cluster_df[(cluster_df['pos'] == pos) & (cluster_df['team_clu_id'] == 2) & (cluster_df['player_clu_id'] == player_id)].iloc[0]
+            print(f"Cluster {player_id}", "Lenght", plyr_clu['length'], "Median", plyr_clu['median'],"STD", plyr_clu['std'])
 
     
 
